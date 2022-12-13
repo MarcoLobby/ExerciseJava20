926bdf37f647e6fce3432f22cbef5b00875badf2
Marco Lo Bello <xSalamander@outlook.it>
2022-12-13 15:57:57 +0100

    Initial Commit

---
+++ src/it/DevelHope/ExerciseJava20/PrintYourNameAndPi.java
@@ -0,0 +1,9 @@
+package it.DevelHope.ExerciseJava20;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "Marco Lo Bello";
+        System.out.printf("Name: %S %n Number Pi: %f", name, 3.141590);
+
+    }
+}
